64f837e55ff8de8a1420edebfdb6ad273d238145
timothyebil <ebiltim2012@gmail.com>
2026-09-08 23:51:46 +0300

    Finished prep 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -11,7 +11,7 @@ class Program
         int userNumber = -1;
         while (userNumber != 0)
         {
-            Console.Write("Enter a number (0 to quit): ");
+            Console.Write("Enter a number (0 to quit): "); 
             
             string userResponse = Console.ReadLine();
             userNumber = int.Parse(userResponse);
